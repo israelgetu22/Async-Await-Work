c25e60edeb69c47cfd49500eda70ff2858aff753
Isarel Getu <israelgetu101@gmail.com>
2023-01-20 21:20:35 -0500

    await is working

--- await.js
+++ await.js
@@ -2,9 +2,38 @@ function makeRequest(location) {
   return new Promise((resolve, reject) => {
     console.log(`Make a request to" ${location}`);
     if (location == "Google") {
-      console.log("Google Say Hi");
+      resolve("Google Say Hi");
     } else {
-      console.log("We are only talk to Google");
+      reject("We are only talk to Google");
     }
   });
 }
+
+function processRequest(response) {
+  return new Promise((resolve, reject) => {
+    console.log("Processing Response");
+    resolve(`Extra Information + ${response}`);
+  });
+}
+
+// makeRequest("Google")
+//   .then((response) => {
+//     return processRequest(response);
+//   })
+//   .then((processRequest) => {
+//     console.log(processRequest);
+//   })
+//   .catch((err) => console.log(err));
+
+async function doWork() {
+  try {
+    const response = await makeRequest("Google");
+    console.log("Response Received");
+    const processedResponse = await processRequest(response);
+    console.log(processedResponse);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+doWork();
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+function processRequest(response) {
+//   return new Promise((resolve, reject) => {
+//     console.log("Processing Response");
+//     resolve(`Extra Information + ${response}`);
+//   });
+// }
+
+// makeRequest("Google")
+//   .then((response) => {
+//     return processRequest(response);
+//   })
+//   .then((processRequest) => {
+//     console.log(processRequest);
+//   })
+//   .catch((err) => console.log(err));
